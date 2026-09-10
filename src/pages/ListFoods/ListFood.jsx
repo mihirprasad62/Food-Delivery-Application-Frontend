5267d91f93f3ed004c9d0c32314cfdad6de29fdd
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2026-09-10 11:16:46 +0530

    fetch foods and delete each food done.

--- src/pages/ListFoods/ListFood.jsx
+++ src/pages/ListFoods/ListFood.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./listFood.css"
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { deleteFood, getFoodList } from '../../services/foodService'
 
 const ListFood = () => {
+  const [list, setList] = useState([])
+
+  const fetchList = async () => {
+    try {
+      const data = await getFoodList()
+      setList(data)
+    } catch (error) {
+      toast.error("error while reading the foods.")
+    }
+  }
+
+  const removeFood = async (foodId) => {
+    try {
+      const success = await deleteFood(foodId)
+      if (success) {
+        toast.success('food removed')
+        await fetchList()
+      } else {
+        toast.error('error occured removing the food.')
+      }
+    } catch (error) {
+      toast.error('error occured removing the food.')
+    }
+
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
   return (
-    <div>ListFood</div>
+    <>
+      <div className='table'>
+        <table border={2}>
+          <thead>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>price</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            {
+              list.map((item, index) => (
+                <tr key={item.id}>
+                  <td>
+                    <img src={item.imageUrl} alt="" height={48} width={48} />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>{item.category}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <button onClick={() => removeFood(item.id)}>delete </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
--- src/services/foodService.js
+++ src/services/foodService.js
@@ -17,3 +17,22 @@ export const addFood = async (foodData, image) => {
     }
 }
 
+export const getFoodList= async ()=>{
+    try {
+        const response =await axios.get(API_URL)
+        return response.data
+    } catch (error) {
+        console.log("Error fetching food list",error)
+        throw error
+    }
+}
+
+export const deleteFood=async (foodId)=>{
+    try {
+       const response= await axios.delete(API_URL+"/"+foodId)
+       return response.status===204
+    } catch (error) {
+        console.log("Error while deleting the food")
+    }
+}
+
