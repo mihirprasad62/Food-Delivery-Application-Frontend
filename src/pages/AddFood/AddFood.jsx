373e95c7a578c7496640fb91380a852409d0c980
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2026-09-19 21:18:27 +0530

    some cate gory added to add product page and orders page done.

--- src/pages/AddFood/AddFood.jsx
+++ src/pages/AddFood/AddFood.jsx
@@ -6,94 +6,100 @@ import { addFood } from '../../services/foodService';
 import { toast } from 'react-toastify';
 
 const AddFood = () => {
-    const[image,setImage]=useState(null)
-     const[data,setData]=useState({
-        name:'',
-        description:'',
-        price:'',
-        category:'biriyani'
-     })
+    const [image, setImage] = useState(null)
+    const [data, setData] = useState({
+        name: '',
+        description: '',
+        price: '',
+        category: 'Rolls'
+    })
 
-     const onChangeHandler=(e)=>{
-        const name=e.target.name;
-        const value=e.target.value;
-        setData(data=>({...data,[name]:value}))
+    const onChangeHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setData(data => ({ ...data, [name]: value }))
 
-     }
+    }
 
-     useEffect(()=>{
+    useEffect(() => {
         console.log(data)
-     },[data])
+    }, [data])
 
-     const handleSubmitHandler=async (e)=>{
+    const handleSubmitHandler = async (e) => {
         e.preventDefault();
-        if(!image){
+        if (!image) {
             toast.error("please upload image")
         }
-        const formData=new FormData()
-        formData.append("food",JSON.stringify(data))
-        formData.append("file",image)
+        const formData = new FormData()
+        formData.append("food", JSON.stringify(data))
+        formData.append("file", image)
 
         try {
-            await addFood(data,image)
-          
-                toast.success("food added successfully")
-                setData({name:'',description:'', price:'', category:'biriyani'})
-                setImage(null)
-            
+            await addFood(data, image)
+
+            toast.success("food added successfully")
+            setData({ name: '', description: '', price: '', category: 'Rolls' })
+            setImage(null)
+
         } catch (error) {
-            console.log("Error in uploading food:::::::",error)
+            console.log("Error in uploading food:::::::", error)
             toast.error("error in adding food")
         }
-     }
-  return (
-    <>
-    <div className='form'>
-     <form action="" onSubmit={handleSubmitHandler}>
-        <h1>ADD FOOD</h1>
-        {/* image  */}
-        <div>
-            <label htmlFor="img">
-                <img src={image? URL.createObjectURL(image):assets.upload_area} width={98} alt="" />
-            </label>
-            <input type="file" id='image' onChange={(e)=>setImage(e.target.files[0])} />
-        </div>
-        {/* name  */}
-        <div>
-            <label htmlFor="name">Name</label>
-        <input type="text" name='name' id='name' onChange={onChangeHandler} value={data.name} />
-        </div>
-        {/* description  */}
-        <div>
-            <label htmlFor="description">Description</label>
-            <textarea  name='description' id='description' rows={4} onChange={onChangeHandler} value={data.description} />
-        </div>
-        {/* category  */}
-        <div>
-            <label htmlFor="category">Category</label>
-       
-       <select name="category" id="category" onChange={onChangeHandler} value={data.category}>
-        <option value="Biriyani">Biriyani</option>
-         <option value="Pizza">Pizza</option>
-          <option value="Salad">Salad</option>
+    }
+    return (
+        <>
+            <div className='form'>
+                <form action="" onSubmit={handleSubmitHandler}>
+                    <h1>ADD FOOD</h1>
+                    {/* image  */}
+                    <div>
+                        <label htmlFor="img">
+                            <img src={image ? URL.createObjectURL(image) : assets.upload_area} width={98} alt="" />
+                        </label>
+                        <input type="file" id='image' onChange={(e) => setImage(e.target.files[0])} />
+                    </div>
+                    {/* name  */}
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name='name' id='name' onChange={onChangeHandler} value={data.name} />
+                    </div>
+                    {/* description  */}
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <textarea name='description' id='description' rows={4} onChange={onChangeHandler} value={data.description} />
+                    </div>
+                    {/* category  */}
+                    <div>
+                        <label htmlFor="category">Category</label>
 
-       </select>
-        </div>
-        {/* price  */}
-        <div>
-            <label htmlFor="price">price</label>
-        <input type="number" name='price' id='price' onChange={onChangeHandler} value={data.price} />
-        </div>
-        {/* submit   */}
-         <div>
-           
-        <input type="submit" value={"save"} />
-        </div>
-    </form>
-    </div>
-   
-    </>
-  )
+                        <select name="category" id="category" onChange={onChangeHandler} value={data.category}>
+                            <option value="Rolls">Rolls</option>
+                            <option value="Deserts">Deserts</option>
+                            <option value="Salad">Salad</option>
+                            <option value="Sandwich">Sandwich</option>
+                            <option value="Cake">Cake</option>
+                            <option value="Pure Veg">Pure Veg</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Noodles">Noodles</option>
+
+
+                        </select>
+                    </div>
+                    {/* price  */}
+                    <div>
+                        <label htmlFor="price">price</label>
+                        <input type="number" name='price' id='price' onChange={onChangeHandler} value={data.price} />
+                    </div>
+                    {/* submit   */}
+                    <div>
+
+                        <input type="submit" value={"save"} />
+                    </div>
+                </form>
+            </div>
+
+        </>
+    )
 }
 
 export default AddFood
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { assets } from '../../assets/admin_assets/assets'
 
 const Orders = () => {
+  const [data,setData]=useState([])
+
+  const fetchOrders=async ()=>{
+   const response=await axios.get("http://localhost:8080/api/orders/all")
+   setData(response.data)
+  }
+
+  const updateStatus=async (e,orderId)=>{
+   const response=await axios.patch(`http://localhost:8080/api/orders/status/${orderId}?status=${e.target.value}`)
+   if(response.status==200){
+    await fetchOrders()
+   }
+  }
+
+  useEffect(()=>{
+    fetchOrders()
+  },[])
   return (
-    <div>Orders</div>
+    <>
+    <table>
+                <thead>
+                  
+                </thead>
+                <tbody>
+                    {
+                        data.map((order, index) => (
+                            <tr key={index}>
+                                <td>
+                                    <img src={assets.bag_icon} height={48} width={48} alt="" />
+                                </td>
+                                <td>
+                                    <div>
+                                      {
+                                        order.orderedItems.map((item,index)=>{
+                                            if(index===order.orderedItems.length-1){
+                                                return item.name+" x "+item.quantity; 
+                                            }else{
+                                                 return item.name+" x "+item.quantity + " "; 
+                                            }
+                                        })
+                                    }
+                                    </div>
+                                    <div>
+                                      {
+                                        order.userAddress
+                                      }
+                                    </div>
+                                </td>
+                                <td>
+                                    {
+                                        order.amount
+                                    }
+                                </td>
+                                <td>Items : {order.orderedItems.length}</td>
+
+                                
+
+                                <td>
+                                    <form action="">
+                                      <select onChange={(event)=>updateStatus(event,order.id)} value={order.orderStatus}>
+                                        <option value="food preparing">Food Preparing</option>
+                                        <option value="out for delivery">out for delivery</option>
+                                        <option value="delivered">delivered</option>
+                                      </select>
+                                    </form>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+    </>
   )
 }
 
